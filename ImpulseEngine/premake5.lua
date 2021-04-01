2da1a49caf940da85137a03432c4c7eecffd619f
da772 <daredano6@hotmail.com>
2021-04-01 15:22:01 -0700

    Update premake5.lua

--- ImpulseEngine/premake5.lua
+++ ImpulseEngine/premake5.lua
@@ -14,6 +14,7 @@ newoption {
 	description = "build as standalone server"
 }
 
+
 IncludeDir = {}
 IncludeDir["GLFW"] = "ImpulseEngine/vendor/GLFW/include"
 IncludeDir["ENET"] = "ImpulseEngine/vendor/Enet/include"
